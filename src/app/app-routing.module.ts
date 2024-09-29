7e817edf4458a1730b54c01d89a3afe0e11ceb40
Darth_Idimmu_pc <dafquin@hotmail.com>
2024-09-28 20:38:39 -0500

    configure_path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { PersonFormComponent } from './person-form/person-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'tasks', component: TaskListComponent },
+  { path: 'create-task', component: TaskFormComponent },
+  { path: 'create-person', component: PersonFormComponent },
+  { path: '', redirectTo: '/tasks', pathMatch: 'full' },
+  { path: '**', redirectTo: '/tasks' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
