1156d9b4088c101355a133e8326ad2096d0b113a
Darth_Idimmu_pc <dafquin@hotmail.com>
2024-09-28 20:39:14 -0500

    add_confugure_appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { PersonFormComponent } from './person-form/person-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskFormComponent,
+    TaskListComponent,
+    PersonFormComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
